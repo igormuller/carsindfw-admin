bde952f944f9d0a20e9359caec0673febc11dfe2
Igor Muller <igor.muller87@gmail.com>
2020-05-15 11:23:59 -0300

    Corrigido apresentacao dos anuncios redimencionando

--- src/views/Site/Home.vue
+++ src/views/Site/Home.vue
@@ -41,77 +41,51 @@
       </v-content>
     </v-container>
     <v-container fluid>
-      <div class="d-inline-flex">
-        <v-card class="mx-auto my-12" max-width="374">
-          <v-img
-            height="250"
-            src="https://image.webmotors.com.br/_fotos/anunciousados/gigante/2020/202003/20200317/bmw-535i-3.0-m-sport-24v-gasolina-4p-automatico-wmimagem19012339042.jpg?s=fill&w=1920&h=1440&q=75"
-          ></v-img>
+      <v-content>
+        <v-row>
+          <v-col v-for="n in 10" xl="2" lg="3" md="4" sm="6" xs="12" :key="n">
+            <v-card max-width="374" height="550">
+              <v-img
+                height="250"
+                src="https://image.webmotors.com.br/_fotos/anunciousados/gigante/2020/202003/20200317/bmw-535i-3.0-m-sport-24v-gasolina-4p-automatico-wmimagem19012339042.jpg?s=fill&w=1920&h=1440&q=75"
+              ></v-img>
 
-          <v-card-title>BMW 535i</v-card-title>
+              <v-card-title>BMW 535i</v-card-title>
 
-          <v-card-text>
-            <v-row align="center" class="mx-0">
-              <v-rating
-                :value="4.5"
-                color="amber"
-                dense
-                half-increments
-                readonly
-                size="14"
-              ></v-rating>
-              <div class="grey--text ml-4">4.5 (413)</div>
-            </v-row>
-            <div class="my-4 subtitle-1">
-              $ 1,500.00
-            </div>
-            <div>
-              Dual-stage suppl. restraint system (SRS) certified to the advanced
-              airbag requiremnets of FMVSS 208, S14.
-            </div>
-          </v-card-text>
+              <v-card-text>
+                <v-row class="mx-0">
+                  <v-rating
+                    :value="4.5"
+                    color="amber"
+                    dense
+                    half-increments
+                    readonly
+                    size="14"
+                  ></v-rating>
+                  <div class="grey--text ml-4">4.5 (413)</div>
+                </v-row>
+                <div class="my-4 subtitle-1">
+                  $ 1,500.00
+                </div>
+                <div>
+                  Dual-stage suppl. restraint system (SRS) certified to the advanced
+                  airbag requiremnets of FMVSS 208, S14.
+                </div>
+              </v-card-text>
 
-          <v-card-actions>
-            <v-btn color="deep-purple lighten-2" text>
-              Reserve
-            </v-btn>
-          </v-card-actions>
-        </v-card>
-
-        <v-card class="ml-5 my-12" max-width="374">
-          <v-img
-            height="250"
-            src="https://autopolis.files.wordpress.com/2012/08/acura-integra-special-edition-1988.jpg?w=547"
-          ></v-img>
-
-          <v-card-title>ACURA Integra</v-card-title>
-
-          <v-card-text>
-            <v-row align="center" class="mx-0">
-              <v-rating
-                :value="4.5"
-                color="amber"
-                dense
-                half-increments
-                readonly
-                size="14"
-              ></v-rating>
-
-              <div class="grey--text ml-4">4.5 (413)</div>
-            </v-row>
-            <div class="my-4 subtitle-1">
-              $ 800.00
-            </div>
-            <div>Driver Seat Only</div>
-          </v-card-text>
-
-          <v-card-actions>
-            <v-btn color="deep-purple lighten-2" text>
-              Reserve
-            </v-btn>
-          </v-card-actions>
-        </v-card>
-      </div>
+              <v-card-actions>
+                <v-row>
+                  <v-col align="end">
+                    <v-btn color="primary" text>
+                      Reserve
+                    </v-btn>
+                  </v-col>
+                </v-row>
+              </v-card-actions>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-content>
     </v-container>
   </v-app>
 </template>
