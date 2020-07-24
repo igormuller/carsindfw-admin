eb2bdc37ef539b9dbadf4c76ddf80ca845fcd6e7
Igor Muller <igor.muller87@gmail.com>
2020-07-24 10:45:10 -0300

    Ajustado home e car detail

--- src/views/Site/CarDetail.vue
+++ src/views/Site/CarDetail.vue
@@ -1,133 +1,42 @@
 <template>
   <div>
-    <v-row>
-      <v-col cols="12" md="8">
-        <v-card>
-          <v-container>
-            <div class="text-center">
-              <h3>2009 BMW - PASSENGER CAR</h3>
-            </div>
-            <v-simple-table dense class="ma-5">
-              <tbody>
-                <tr>
-                  <th>Manufacturer:</th>
-                  <td>BMW AG</td>
-                </tr>
-                <tr>
-                  <th>Vehicle Type:</th>
-                  <td>PASSENGER CAR</td>
-                </tr>
-                <tr>
-                  <th>Model Year:</th>
-                  <td>2009</td>
-                </tr>
-                <tr>
-                  <th>Make:</th>
-                  <td>BMW</td>
-                </tr>
-                <tr>
-                  <th>Model:</th>
-                  <td>535i</td>
-                </tr>
-                <tr>
-                  <th>Body Class:</th>
-                  <td>Sedan/Saloon</td>
-                </tr>
-              </tbody>
-            </v-simple-table>
-          </v-container>
-        </v-card>
-      </v-col>
-      <v-col cols="12" md="4">
-        <v-card>
-          <v-container>
-            <v-row>
-              <v-col v-for="n in 9" :key="n" class="d-flex child-flex" cols="4">
-                <v-card flat tile class="d-flex">
-                  <v-img
-                    @click="index = 1"
-                    :src="`https://picsum.photos/500/300?image=${n * 5 + 10}`"
-                    :lazy-src="`https://picsum.photos/10/6?image=${n * 5 + 10}`"
-                    aspect-ratio="1"
-                    class="grey lighten-2"
-                  >
-                    <template v-slot:placeholder>
-                      <v-row
-                        class="fill-height ma-0"
-                        align="center"
-                        justify="center"
-                      >
-                        <v-progress-circular
-                          indeterminate
-                          color="grey lighten-5"
-                        >
-                        </v-progress-circular>
-                      </v-row>
-                    </template>
-                  </v-img>
-                </v-card>
-              </v-col>
-            </v-row>
-          </v-container>
-        </v-card>
-      </v-col>
-    </v-row>
-    <v-row>
-      <v-col>
-        <gallery :images="images" :index="index" @close="index = null" :options="{closeOnSlideClick: true}"></gallery>
-        <v-card>
-          <v-card-title>
-            Cars this Dealer
-          </v-card-title>
-          <v-container fluid>
-            <v-row dense>
-              <v-col
-                v-for="(card, index) in cards"
-                :key="index"
-                cols="12"
-                sm="4"
-                :md="card.flex"
-              >
-                <v-card>
-                  <v-img
-                    :src="card.src"
-                    class="white--text align-end"
-                    height="200px"
-                  >
-                    <v-card-title v-text="card.title"></v-card-title>
-                  </v-img>
-                  <v-card-actions>
-                    <v-spacer></v-spacer>
+    <v-sheet class="mx-auto" elevation="5" max-width="800">
+      <v-slide-group v-model="model" class="px-5" show-arrows>
+        <v-slide-item
+          v-for="n in cards"
+          :key="n"
+          v-slot:default="{ active, toggle }"
+        >
+          <v-card
+            :color="active ? 'primary' : 'default'"
+            class="ma-2"
+            height="100"
+            width="100"
+            @click="toggle"
+          >
+            <v-img
+              class="mt-2 ml-2"
+              height="85"
+              width="85"
+              :src="n.src"
+            ></v-img>
+          </v-card>
+        </v-slide-item>
+      </v-slide-group>
 
-                    <v-btn icon>
-                      <v-icon>mdi-heart</v-icon>
-                    </v-btn>
-
-                    <v-btn icon>
-                      <v-icon>mdi-bookmark</v-icon>
-                    </v-btn>
-
-                    <v-btn icon>
-                      <v-icon>mdi-share-variant</v-icon>
-                    </v-btn>
-                  </v-card-actions>
-                </v-card>
-              </v-col>
-            </v-row>
-          </v-container>
-        </v-card>
-      </v-col>
-    </v-row>
+      <v-expand-transition>
+        <v-sheet v-if="model != null" color="grey lighten-4" height="500" tile>
+          <v-img max-height="500" :src="cards[model].src"></v-img>
+        </v-sheet>
+      </v-expand-transition>
+    </v-sheet>
   </div>
 </template>
 
 <script>
-import VueGallery from 'vue-gallery';
 export default {
-  components: {
-    "gallery": VueGallery
-  },
   data: () => ({
+    model: null,
     index: null,
     cards: [
       {
@@ -159,13 +68,22 @@ export default {
         title: "Best airlines",
         src: "https://cdn.vuetifyjs.com/images/cards/plane.jpg",
         flex: 2
+      },
+      {
+        title: "Pre-fab homes",
+        src: "https://cdn.vuetifyjs.com/images/cards/house.jpg",
+        flex: 2
+      },
+      {
+        title: "Favorite road trips",
+        src: "https://cdn.vuetifyjs.com/images/cards/road.jpg",
+        flex: 2
+      },
+      {
+        title: "Best airlines",
+        src: "https://cdn.vuetifyjs.com/images/cards/plane.jpg",
+        flex: 2
       }
-    ],
-    images: [
-      "https://dummyimage.com/800/ffffff/000000",
-      "https://dummyimage.com/1600/ffffff/000000",
-      "https://dummyimage.com/1280/000000/ffffff",
-      "https://dummyimage.com/400/000000/ffffff"
     ]
   })
 };
--- src/views/Site/Home.vue
+++ src/views/Site/Home.vue
@@ -78,43 +78,14 @@
     </div>
     <v-row>
       <v-col v-for="n in 4" xl="2" lg="3" md="4" sm="6" xs="12" :key="n">
-        <v-card max-width="374" height="550">
+        <v-card to="car-detail" width="500" class="pr-2">
           <v-img
-            height="250"
             src="https://image.webmotors.com.br/_fotos/anunciousados/gigante/2020/202003/20200317/bmw-535i-3.0-m-sport-24v-gasolina-4p-automatico-wmimagem19012339042.jpg?s=fill&w=1920&h=1440&q=75"
-          ></v-img>
-
-          <v-card-title>BMW 535i</v-card-title>
-
-          <v-card-text>
-            <v-row class="mx-0">
-              <v-rating
-                :value="4.5"
-                color="amber"
-                dense
-                half-increments
-                readonly
-                size="14"
-              ></v-rating>
-              <div class="grey--text ml-4">4.5 (413)</div>
-            </v-row>
-            <div class="my-4 subtitle-1">
-              $ 1,500.00
-            </div>
-            <div>
-              Dual-stage suppl. restraint system (SRS) certified to the advanced
-              airbag requiremnets of FMVSS 208, S14.
-            </div>
-          </v-card-text>
-
+            height="200"
+          >
+          </v-img>
           <v-card-actions>
-            <v-row>
-              <v-col align="end">
-                <v-btn color="primary" text to="car-detail">
-                  Reserve
-                </v-btn>
-              </v-col>
-            </v-row>
+            Dealer
           </v-card-actions>
         </v-card>
       </v-col>
