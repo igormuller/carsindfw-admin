ff2c212b5789d06522c43d20b4cc24674c0cbcf1
Igor Muller <igor.muller87@gmail.com>
2020-10-19 11:03:28 -0300

    Incluido imagem de fundo no dealers

--- src/views/Site/Dealer/List.vue
+++ src/views/Site/Dealer/List.vue
@@ -1,6 +1,22 @@
 <template>
-  <v-card>
-    List Dealers
+  <v-card class="py-5">
+    <v-img
+      src="@/assets/sem_fundo.png"
+      max-height="500"
+      style="opacity: 0.8"
+      contain
+    >
+      <v-list-item
+        v-for="(city, i) in cities"
+        :key="i"
+        :to="city"
+        class="ml-12"
+      >
+        <v-list-item-content>
+          <v-list-item-title>{{ city }}</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+    </v-img>
   </v-card>
 </template>
 
@@ -8,11 +24,11 @@
 export default {
   data() {
     return {
-      dealers: []
+      cities: []
     };
   },
   created() {
-    // this.$http.get("/dealers");
+    this.$http.get("/cities-dealers");
   }
 };
 </script>
