37b46b7cc435c207c52ce7a256b7c70b5ee3030c
Igor Muller <igor.muller87@gmail.com>
2020-11-17 00:59:54 -0300

    Feitos ajustes no detalhe do carro e interesse

--- src/views/Site/CarDetail/CarDetail.vue
+++ src/views/Site/CarDetail/CarDetail.vue
@@ -4,13 +4,11 @@
       <BannerTop :slides="slides"></BannerTop>
     </v-card>
     <v-row>
-      <v-col cols="12" md="3" xl="3">
+      <v-col cols="12" md="3">
         <v-card>
           <v-img
             src="https://cdn.vuetifyjs.com/images/cards/house.jpg"
             class="white--text align-end"
-            height="200px"
-            width="300px"
           >
             <v-card-title text="card.title">Anúncio</v-card-title>
           </v-img>
@@ -21,36 +19,72 @@
           </v-card-text>
         </v-card>
       </v-col>
-      <v-col cols="12" md="7" xl="7">
+      <v-col cols="12" md="6">
         <Galery
           v-if="advertisement.gallery"
           :images="advertisement.gallery"
         ></Galery>
-        <v-card class="mt-3">
-          <AboutCar :carDetail="advertisement"></AboutCar>
-        </v-card>
+        <AboutCar :carDetail="advertisement"></AboutCar>
       </v-col>
-      <v-col cols="12" md="2" xl="2">
-        <v-card class="mb-3">
+      <v-col cols="12" md="3">
+        <!-- <v-card class="mb-3">
           <v-card-text>
             <p><v-icon class="mb-1" color="red">star_rate</v-icon> Save</p>
             <p><v-icon class="mb-1" color="red">list</v-icon> My List</p>
             <p><v-icon class="mb-1" color="red">mail_outline</v-icon> E-mail</p>
           </v-card-text>
-        </v-card>
-        <v-card>
+        </v-card> -->
+        <v-card
+          v-if="!interest_sended"
+          :loading="loading_send"
+          :disabled="loading_send"
+        >
           <v-card-title class="text--center">Make Your Offer</v-card-title>
           <v-card-text>
-            <v-text-field label="Name" dense></v-text-field>
-            <v-text-field label="Phone" dense></v-text-field>
-            <v-text-field label="E-mail" dense></v-text-field>
-            <v-checkbox dense label="Recived an e-mail copy"></v-checkbox>
-            <v-textarea label="Proposal" rows="3" dense></v-textarea>
+            <v-text-field
+              label="Name"
+              dense
+              v-model="interest.name"
+              :error-messages="errors.name"
+            ></v-text-field>
+            <v-text-field
+              label="Phone"
+              dense
+              v-model="interest.phone"
+              :error-messages="errors.phone"
+            ></v-text-field>
+            <v-text-field
+              label="E-mail"
+              dense
+              v-model="interest.email"
+              :error-messages="errors.email"
+            ></v-text-field>
+            <v-checkbox
+              label="Recived an e-mail copy"
+              dense
+              v-model="interest.copy"
+            ></v-checkbox>
+            <v-textarea
+              label="Proposal"
+              rows="3"
+              dense
+              v-model="interest.description"
+              :error-messages="errors.description"
+            ></v-textarea>
           </v-card-text>
           <v-card-actions>
-            <v-spacer></v-spacer>
-            <v-btn color="#00205b" class="white--text">Send</v-btn>
+            <v-btn
+              color="#00205b"
+              class="white--text"
+              @click="sendProposal()"
+              block
+            >
+              Send
+            </v-btn>
           </v-card-actions>
+        </v-card>
+        <v-card v-else class="text-center py-10 px-2">
+          <h2 style="color:#00205b">Thanks for your offer</h2>
         </v-card>
       </v-col>
     </v-row>
@@ -68,11 +102,33 @@ export default {
   props: ["id"],
   data: () => ({
     advertisement: {},
+    interest: {},
+    interest_sended: false,
+    loading_send: false,
+    errors: [],
     slides: [
       { src: require("@/assets/site/car-wash.png") },
       { src: "https://cdn.vuetifyjs.com/images/cards/road.jpg" }
     ]
   }),
+  methods: {
+    async sendProposal() {
+      this.interest.advertisement_id = this.id;
+      this.loading_send = true;
+      await this.$http
+        .post(`/register-interest`, this.interest)
+        .then(() => (this.interest_sended = !this.interest_sended))
+        .catch(error => {
+          console.log(error.response.data.message);
+          this.errors = error.response.data.errors;
+          this.loading_send = false;
+
+          this.$toasted.global.defaultError({
+            msg: error.response.data.message
+          });
+        });
+    }
+  },
   created() {
     this.$http
       .get(`/car-detail/${this.id}`)
--- src/views/Site/CarDetail/Components/AboutCar.vue
+++ src/views/Site/CarDetail/Components/AboutCar.vue
@@ -1,40 +1,73 @@
 <template>
   <div>
-    <v-card-title style="color:#00205b"><h2>About this Car</h2></v-card-title>
-    <v-card-text>
-      <strong>
-        {{ carDetail.make_name }} - {{ carDetail.model_name }} -
-        {{ carDetail.trim }}
-      </strong>
-      <v-row class="ml-5">
-        <v-col>
-          <p><strong>Engine</strong></p>
-          <p>{{ carDetail.engine }}</p>
-        </v-col>
-        <v-col>
-          <p><strong>MPG Fuel Type</strong></p>
-          <p>{{ carDetail.fuel_type_front }}</p>
-        </v-col>
-        <v-col>
-          <p><strong>Transmission</strong></p>
-          <p>{{ carDetail.transmission_front }}</p>
-        </v-col>
-      </v-row>
-      <v-row class="ml-5">
-        <v-col>
-          <p><strong>Seats</strong></p>
-          <p>{{ carDetail.seats }}</p>
-        </v-col>
-        <v-col>
-          <p><strong>Exterior Color</strong></p>
-          <p>{{ carDetail.color_ext }}</p>
-        </v-col>
-        <v-col>
-          <p><strong>Drivetrain</strong></p>
-          <p>{{ carDetail.drive_type }}</p>
-        </v-col>
-      </v-row>
-    </v-card-text>
+    <v-card class="mt-3">
+      <v-card-title style="color:#00205b"><h2>About this Car</h2></v-card-title>
+      <v-card-text>
+        <strong>
+          {{ carDetail.make_name }} - {{ carDetail.model_name }} -
+          {{ carDetail.trim }}
+        </strong>
+        <v-row align="center">
+          <v-col class="d-flex flex-column align-center">
+            <v-img
+              src="@/assets/site/car-detail/engine.jpeg"
+              height="50"
+              width="50"
+            ></v-img>
+            <p>{{ carDetail.engine }}</p>
+          </v-col>
+          <v-col class="d-flex flex-column align-center">
+            <v-img
+              src="@/assets/site/car-detail/fuel-type.jpeg"
+              height="50"
+              width="50"
+            ></v-img>
+            <p>{{ carDetail.fuel_type_front }}</p>
+          </v-col>
+          <v-col class="d-flex flex-column align-center">
+            <v-img
+              src="@/assets/site/car-detail/transmission.jpeg"
+              height="50"
+              width="50"
+            ></v-img>
+            <p>{{ carDetail.transmission_front }}</p>
+          </v-col>
+        </v-row>
+        <v-row align="center">
+          <v-col class="d-flex flex-column align-center">
+            <v-img
+              src="@/assets/site/car-detail/seats.jpeg"
+              height="50"
+              width="50"
+            ></v-img>
+            <p>{{ carDetail.seats }}</p>
+          </v-col>
+          <v-col class="d-flex flex-column align-center">
+            <v-img
+              src="@/assets/site/car-detail/color.jpeg"
+              height="50"
+              width="50"
+            ></v-img>
+            <p>{{ carDetail.color_ext }}</p>
+          </v-col>
+          <v-col class="d-flex flex-column align-center">
+            <v-img
+              src="@/assets/site/car-detail/drivetrain.jpeg"
+              height="50"
+              width="50"
+            ></v-img>
+            <p>{{ carDetail.drive_type }}</p>
+          </v-col>
+        </v-row>
+      </v-card-text>
+    </v-card>
+    <v-card
+      class="mt-3 pa-3 text-center"
+      color="#acc0e3"
+      style="color: #00205b;"
+    >
+      <h1>{{ carDetail.value }}</h1>
+    </v-card>
   </div>
 </template>
 
