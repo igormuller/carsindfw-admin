26494d9c92df5198b1c312c5288206b5564ec1b5
Igor Muller <igor.muller87@gmail.com>
2021-06-29 11:46:57 -0300

    Padrão news

--- src/views/Site/News.vue
+++ src/views/Site/News.vue
@@ -3,8 +3,7 @@
     <v-card-title>{{ new_blog.title }}</v-card-title>
     <v-card-subtitle>{{ new_blog.sub_title }}</v-card-subtitle>
     <v-card-text>
-      <!-- <div class="ma-5" v-html="new_blog.body"></div> -->
-      <h3>Content of the editor.</h3> <h2>Mais um teste</h2><br><strong>Op&ccedil;&otilde;es:</strong> <ul> <li><strong>Teste 1</strong></li> <li>Teste 2</li> <li>Teste 3</li> </ul> <blockquote> <p>Mis um teste</p> </blockquote>
+      <div class="ma-5" v-html="new_blog.body"></div>
     </v-card-text>
   </v-card>
 </template>
